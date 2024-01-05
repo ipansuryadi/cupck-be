d9d82445b045607460db5adcd8b49cb8f926b6be
Ipan Suryadi <ipansuryadi@gmail.com>
2024-01-05 20:58:41 +0700

    feat: init

---
+++ src/main/kotlin/com/cupcake/learn/be/CupcakeBeApplication.kt
@@ -0,0 +1,11 @@
+package com.cupcake.learn.be
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CupcakeBeApplication
+
+fun main(args: Array<String>) {
+	runApplication<CupcakeBeApplication>(*args)
+}
---
+++ src/test/kotlin/com/cupcake/learn/be/CupcakeBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cupcake.learn.be
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CupcakeBeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
