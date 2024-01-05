a7339b032f552d16b1d9c52fcf165b80c0327c14
Ipan Suryadi <ipansuryadi@gmail.com>
2024-01-05 21:58:13 +0700

    feat: first api get

--- src/main/kotlin/com/cupcake/learn/be/CupcakeBeApplication.kt
+++ src/main/kotlin/com/cupcake/learn/be/CupcakeBeApplication.kt
@@ -1,11 +1,52 @@
 package com.cupcake.learn.be
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.Indexed
+import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.mapping.Field
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.Query
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Repository
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @SpringBootApplication
 class CupcakeBeApplication
 
 fun main(args: Array<String>) {
-	runApplication<CupcakeBeApplication>(*args)
+    runApplication<CupcakeBeApplication>(*args)
+}
+
+
+@Document("person")
+data class Person(
+        @Id
+        val id: String,
+        @Field(name = "name")
+        @Indexed(unique = true)
+        var name: String
+)
+
+@Repository
+interface MainRepository : MongoRepository<Person, String> {
+    @Query("{'name':?0}")
+    fun findByName(name: String): Optional<Person>
+}
+
+@Service
+class MainService(@Autowired val mainRepository: MainRepository) {
+    fun getName(name: String): Person = mainRepository.findByName(name).orElseThrow { throw RuntimeException("Cannot fond name") }
+}
+
+@RestController
+class MainController(@Autowired val mainService: MainService) {
+    @GetMapping("/")
+    fun index(@RequestParam("name") name: String) = ResponseEntity.ok(mainService.getName(name))
 }
